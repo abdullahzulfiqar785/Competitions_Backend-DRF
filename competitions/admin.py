921d414798ffd48aead64daf517e5d5e8e570de3
MianShamasulhaq <70095061+MianShamasulhaq@users.noreply.github.com>
2021-09-03 20:49:45 +0500

    shabash ty tu dani nai

--- competitions/admin.py
+++ competitions/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CompetitionImage, Competition
+from .models import CompetitionImage, Competition, CompetitionTicket
 
 
 class CompetitionImageAdmin(admin.TabularInline):
@@ -34,7 +34,17 @@ class CompetitionAdmin(admin.ModelAdmin):
     )
 
     # def save_model(self, request: Any, obj: _ModelT, form: Any, change: Any) -> None:
-    #     return super().save_model(request, obj, form, change)
-
+    #     return super().save_model(request, obj, form, change)\
+    
     def save_model(self, request, obj, form, change):
+        choices=[chr(i) for i in range(ord(obj.letter_choices[0].upper()),ord(obj.letter_choices[2].upper())+1)]
+        num_choice=lst_int = [int(x) for x in obj.numbers_from.split("-")]
+
+        for i in choices:
+            for j in range(num_choice[0],num_choice[1]):
+                ticket=i+j
+                CompetitionTicket.objects.create(obj,ticket)
+
+
+
         pass
